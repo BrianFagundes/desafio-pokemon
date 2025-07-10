4323d1d4e4a08f319fcb0d303129943809149682
Brian Fagundes de Moura <brianmoura.f@gmail.com>
2025-07-10 20:34:26 -0300

    funcionalidade: Define os modelos Pokemon e Tipo

--- catalogo_pokemon/core/models.py
+++ catalogo_pokemon/core/models.py
@@ -1,3 +1,30 @@
+# core/models.py
+
 from django.db import models
 
-# Create your models here.
+class Tipo(models.Model):
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome")
+
+    def __str__(self):
+        return self.nome
+
+class Pokemon(models.Model):
+    codigo = models.IntegerField(primary_key=True, verbose_name="Código")
+    nome = models.CharField(max_length=100, verbose_name="Nome")
+    tipo_primario = models.ForeignKey(Tipo, on_delete=models.PROTECT, related_name='pokemons_primarios', verbose_name="Tipo Primário")
+    tipo_secundario = models.ForeignKey(
+        Tipo,
+        on_delete=models.PROTECT,
+        related_name='pokemons_secundarios',
+        null=True,
+        blank=True,
+        verbose_name="Tipo Secundário"
+    )
+
+    class Meta:
+        verbose_name = "Pokemon"
+        verbose_name_plural = "Pokemons"
+        ordering = ['codigo']
+
+    def __str__(self):
+        return f"#{self.codigo:03} - {self.nome}"
