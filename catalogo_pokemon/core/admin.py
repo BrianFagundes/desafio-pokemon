1a580558326d382fdd571ad1e67867da828f96ff
Brian Fagundes de Moura <brianmoura.f@gmail.com>
2025-07-10 20:41:00 -0300

    funcionalidade: Configura o Admin para os modelos Pokemon e Tipo

--- catalogo_pokemon/core/admin.py
+++ catalogo_pokemon/core/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# core/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Tipo, Pokemon
+
+@admin.register(Tipo)
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nome', 'tipo_primario', 'tipo_secundario')
+    search_fields = ('nome', 'codigo')
+    list_filter = ('tipo_primario', 'tipo_secundario')
