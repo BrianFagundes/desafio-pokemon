cf0682d011b0aa8168d807f3da55bafafa67cd33
Brian Fagundes de Moura <brianmoura.f@gmail.com>
2025-07-10 22:15:14 -0300

    funcionalidade: Adiciona CRUD público completo para Pokémons e Tipos

--- catalogo_pokemon/core/urls.py
+++ catalogo_pokemon/core/urls.py
@@ -3,5 +3,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # READ 
     path('', views.lista_pokemons, name='lista_pokemons'),
+    path('tipos/', views.lista_tipos, name='lista_tipos'),
+
+    #CRUD para os pokemons
+    # CREATE
+    path('pokemon/adicionar/', views.PokemonCreateView.as_view(), name='pokemon_adicionar'),
+
+    # UPDATE
+    path('pokemon/<int:pk>/editar/', views.PokemonUpdateView.as_view(), name='pokemon_editar'),
+
+    # DELETE
+    path('pokemon/<int:pk>/deletar/', views.PokemonDeleteView.as_view(), name='pokemon_deletar'),
+
+    #CRUD para os tipos
+    # CREATE
+    path('tipo/adicionar/', views.TipoCreateView.as_view(), name='tipo_adicionar'),
+
+    # UPDATE
+    path('tipo/<int:pk>/editar/', views.TipoUpdateView.as_view(), name='tipo_editar'),
+
+    # DELETE
+    path('tipo/<int:pk>/deletar/', views.TipoDeleteView.as_view(), name='tipo_deletar'),
 ]
--- catalogo_pokemon/core/views.py
+++ catalogo_pokemon/core/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from .models import Pokemon, Tipo
 from django.db.models import Q
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def lista_pokemons(request):
     # Busca inicial de todos os pokemons, ordenados pelo código
@@ -29,3 +31,65 @@ def lista_pokemons(request):
         'tipos': tipos
     }
     return render(request, 'core/lista_pokemons.html', contexto)
+
+def lista_tipos(request):
+    tipos = Tipo.objects.all().order_by('nome')
+    contexto = {'tipos': tipos}
+    return render(request, 'core/lista_tipos.html', contexto)
+
+class PokemonCreateView(CreateView):
+    model = Pokemon
+    fields = ['codigo', 'nome', 'tipo_primario', 'tipo_secundario']
+    template_name = 'core/pokemon_form.html'
+    success_url = reverse_lazy('lista_pokemons')
+
+    def get_initial(self):
+        # Pega os valores iniciais da classe pai (se houver)
+        initial = super().get_initial()
+        
+        # Busca o último Pokemon cadastrado, ordenando pelo maior código
+        ultimo_pokemon = Pokemon.objects.order_by('-codigo').first()
+        
+        if ultimo_pokemon:
+            # Se existe um, o próximo código é o último + 1
+            proximo_codigo = ultimo_pokemon.codigo + 1
+        else:
+            # Se não existe nenhum, começa do 1
+            proximo_codigo = 1
+            
+        # Define o valor inicial para o campo 'codigo'
+        initial['codigo'] = proximo_codigo
+        
+        return initial
+
+#CRUD para os pokemons
+
+class PokemonUpdateView(UpdateView):
+    model = Pokemon
+    fields = ['nome', 'tipo_primario', 'tipo_secundario'] # Não permitimos editar o código
+    template_name = 'core/pokemon_form.html'
+    success_url = reverse_lazy('lista_pokemons')
+
+class PokemonDeleteView(DeleteView):
+    model = Pokemon
+    template_name = 'core/pokemon_confirmar_delete.html'
+    success_url = reverse_lazy('lista_pokemons')
+
+#CRUD para os tipos
+
+class TipoCreateView(CreateView):
+    model = Tipo
+    fields = ['nome'] # Apenas o campo 'nome' é necessário
+    template_name = 'core/tipo_form.html'
+    success_url = reverse_lazy('lista_pokemons') # Pode redirecionar para a lista principal
+
+class TipoUpdateView(UpdateView):
+    model = Tipo
+    fields = ['nome']
+    template_name = 'core/tipo_form.html'
+    success_url = reverse_lazy('lista_pokemons')
+
+class TipoDeleteView(DeleteView):
+    model = Tipo
+    template_name = 'core/tipo_confirmar_delete.html'
+    success_url = reverse_lazy('lista_pokemons')
